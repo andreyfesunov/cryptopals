29019720b6f8166038ee6166cf52bb9ef4eefebf
andreyfesunov <fsnv04@gmail.com>
2026-06-24 18:00:55 +0000

    feat(problem_6): add get KEYSIZE candidates method and test

--- set_1/problem_6/6.go
+++ set_1/problem_6/6.go
@@ -3,6 +3,7 @@ package problem_6
 import (
 	"fmt"
 	"math/bits"
+	"sort"
 )
 
 func GetEditDistance(a, b []byte) (int, error) {
@@ -18,4 +19,39 @@ func GetEditDistance(a, b []byte) (int, error) {
 	}
 
 	return score, nil
+}
+
+type ComparationResult struct {
+	length int
+	value  float64
+}
+
+func GetKeyCandidates(bytes []byte, minLength, maxLength, candidates int) ([]int, error) {
+	results := make([]ComparationResult, 0, maxLength-minLength+1)
+
+	for length := minLength; length <= maxLength; length += 1 {
+		distance, err := GetEditDistance(bytes[:length], bytes[length:2*length])
+
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, ComparationResult{
+			length: length,
+			value:  float64(distance) / float64(length),
+		})
+	}
+
+	sort.Slice(results, func(a, b int) bool {
+		return results[a].value < results[b].value
+	})
+
+	candidates = min(candidates, len(results))
+	answers := make([]int, 0, candidates)
+
+	for _, item := range results[:candidates] {
+		answers = append(answers, item.length)
+	}
+
+	return answers, nil
 }
--- set_1/problem_6/6_test.go
+++ set_1/problem_6/6_test.go
@@ -1,6 +1,9 @@
 package problem_6
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestEditDistance(t *testing.T) {
 	result, err := GetEditDistance([]byte("this is a test"), []byte("wokka wokka!!!"))
@@ -11,4 +14,18 @@ func TestEditDistance(t *testing.T) {
 	if result != 37 {
 		t.Fatal("wrong result", result)
 	}
+}
+
+func TestGetKeyCandidatesSucceeded(t *testing.T) {
+	content, err := os.ReadFile("6.txt")
+	if err != nil {
+		t.Fatal("error on opening file", err)
+	}
+
+	candidates, err := GetKeyCandidates(content, 2, 37, 3)
+	if err != nil {
+		t.Fatal("error on getting candidates", err)
+	}
+
+	t.Log("candidates:", candidates)
 }
